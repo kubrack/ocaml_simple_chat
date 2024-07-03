e16bef8dc29aac7d0d515065e0b9c45197e015a1
Oleksandr Kubrak <kubrack@gmail.com>
2024-07-03 20:13:18 +0200

    seq -> id

--- lib/msg.ml
+++ lib/msg.ml
@@ -1,7 +1,7 @@
 (* Protocol
    +0  1 byte  version
    +1  1 byte  type of msg (M - msg, P - part of msg, A - ack)
-   +2  8 bytes seq number of msg (in chat)
+   +2  8 bytes id of msg (in chat)
    +10 2 bytes data length
    +12 0 .. (2**16 - 12) data
 *)
@@ -21,15 +21,15 @@ let now_us () =
   |> Base.Int63.to_int64 
   |> (fun ns -> Int64.div ns 1000L)
 
-let rtt seq = Int64.sub (now_us ()) seq
-let rtt_s seq = (rtt seq |> Int64.to_int |> float_of_int) /. 1000000.
+let rtt id = Int64.sub (now_us ()) id
+let rtt_s id = (rtt id |> Int64.to_int |> float_of_int) /. 1000000.
 
-let compose mtype seq msg_buf =
+let compose mtype id msg_buf =
   let length = Bytes.length msg_buf in
   let header = Bytes.create proto_data_offset in
   let () = Bytes.set header proto_ver_offset proto_ver in
   let () = Bytes.set header proto_type_offset mtype in
-  let () = Bytes.set_int64_be header proto_time_offset seq in
+  let () = Bytes.set_int64_be header proto_time_offset id in
   let () = Bytes.set_uint16_be header proto_length_offset length in
   Bytes.cat header msg_buf
 
@@ -40,7 +40,7 @@ let extract_header buf =
   (mtype, time, length)
 
 let compose_msg buf = compose proto_type_msg (now_us ()) buf
-let compose_ack seq = compose proto_type_ack seq (Bytes.create 0)
+let compose_ack id = compose proto_type_ack id (Bytes.create 0)
 let msg_id msg = match extract_header msg with (_mtype, time, _length) -> time
 
 let net_msg_fsm reader ack_handler msg_handler =
@@ -66,15 +66,15 @@ let net_msg_fsm reader ack_handler msg_handler =
       | mtype, time, len when mtype = proto_type_msg -> s_msg time len
       | _ -> s_sync ())
     | _ -> s_sync ()
-  and s_ack seq =
+  and s_ack id =
     debug_fn __FUNCTION__;
-    ack_handler seq;
+    ack_handler id;
     Some buf
-  and s_msg seq len =
-    debug_fn (__FUNCTION__ ^ (Core.sprintf " seq [%s] len [%d]" (Int64.to_string seq) len));
+  and s_msg id len =
+    debug_fn (__FUNCTION__ ^ (Core.sprintf " id [%s] len [%d]" (Int64.to_string id) len));
     match reader buf proto_data_offset len with
     | len -> let msg = Bytes.sub buf proto_data_offset len in
-      let () = msg_handler msg seq in 
+      let () = msg_handler msg id in 
       Some msg
   in
   s_sync 
